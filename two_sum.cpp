eea6055553a7567a042b9c681bd1ea97f1ff25fa
Aili Gong <aili.hgong@gmail.com>
2020-05-28 20:27:50 +0000

    Added notes

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <unordered_map>
-#include <map>
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
@@ -10,15 +9,18 @@ vector<int> twoSum(vector<int>& nums, int target) {
         return res;
     }
     
-    unordered_map<int, int> map;
+    unordered_map<int, int> um;
     
     for (int i = 0; i < nums.size(); i++) {
-        if (map.find(target - nums[i]) != map.end()) {
-            res.push_back(map[target - nums[i]]);
+        // auto is used when we do not know what type the um.find returns, in this case auto is int
+        auto f = um.find(target - nums[i]);
+        // if .find() != .end(), then it means FOUND/Contains!
+        if (f != um.end()) {
+            res.push_back(f->second); // first is KEY, second is VALUE;
             res.push_back(i);
             break;
         }
-        map[nums[i]] = i;
+        um[nums[i]] = i;
     }
 
     return res;
