c4dff3843dda7421f77201926417b4163561db71
Aili Gong <aili.hgong@gmail.com>
2020-06-03 23:42:44 +0000

    Added spiral_matrix_II

---
+++ spiral_matrix_II.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> generate_matrix(int n) {
+    vector<vector<int>> matrix(n);
+    for (int i = 0; i < n; i++) {
+        matrix[i].resize(n);
+    }
+
+    if (n == 0) {
+        return matrix;
+    }
+
+    int top = 0;
+    int bottom = n - 1;
+    int left = 0;
+    int right = n - 1;
+
+    int k = 1;
+
+    while (left < right && top < bottom) {
+        for (int i = left; i < right; i++) {
+            matrix[top][i] = k++;
+        }
+        for (int i = top; i < bottom; i++) {
+            matrix[i][right] = k++;
+        }
+        for (int i = right; i > left; i--) {
+            matrix[bottom][i] = k++;
+        }
+        for (int i = bottom; i > top; i--) {
+            matrix[i][left] = k++;
+        }
+
+        left ++;
+        right --;
+        top ++;
+        bottom --;
+    }
+
+    if (n % 2 != 0) {
+        matrix[n / 2][n / 2] = k;
+    }
+
+    return matrix;
+}
