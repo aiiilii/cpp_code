8953f8555d2ea964107fa098762689d4b4b14fc6
Aili Gong <aili.hgong@gmail.com>
2020-06-04 18:32:21 +0000

    Added unordered map solution to subarray_sum_equals_k

--- subarray_sum_equals_k.cpp
+++ subarray_sum_equals_k.cpp
@@ -1,8 +1,31 @@
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-
+// Hashmap approach
+int subarray_sum1(vector<int>& nums, int k) {
+    int count = 0;
+    int sum = 0;
+    
+    if (nums.size() == 0) {
+        return count;
+    }
+    
+    unordered_map<int, int> um;
+    um.emplace(0, 1);
+    
+    for (int i = 0; i < nums.size(); i++) {
+        sum += nums[i];
+        
+        auto it = um.find(sum - k);
+        if (it != um.end()) {
+            count += it->second;
+        }
+        um[sum]++;
+    }
+    return count;
+}
 
 
 
