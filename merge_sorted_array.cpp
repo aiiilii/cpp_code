c5a8bcfc8e7a6f640d2d42ffdb799024c4125392
Aili Gong <aili.hgong@gmail.com>
2020-06-08 18:41:23 +0000

    Added merge_sorted_array

--- merge_sorted_array.cpp
+++ merge_sorted_array.cpp
@@ -2,3 +2,25 @@
 
 using namespace std;
 
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int k = nums1.size() - 1;
+    int i = m - 1;
+    int j = n - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+    while (i >= 0) {
+        nums1[k--] = nums1[i--];
+    }
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
+}
