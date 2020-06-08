18c377920c986ab0cb6867b98d32fcac9405b834
Aili Gong <aili.hgong@gmail.com>
2020-06-08 21:14:36 +0000

    Added reverse_nodes_in_k_group

--- k_closest_points_to_origin.cpp
+++ k_closest_points_to_origin.cpp
@@ -10,7 +10,7 @@ vector<vector<int>> k_closest(vector<vector<int>>& points, int K) {
     }
 
     auto comp = [](const vector<int>& a, const vector<int>& b) {
-        return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
+        return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]); // default maxHeap priority queue
     };
     typedef priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> my_priority_queue_t;
     my_priority_queue_t maxHeap(comp);
---
+++ reverse_nodes_in_k_group.cpp
@@ -0,0 +1,53 @@
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(next) {}
+};
+
+class ReverseK {
+public:
+    ListNode* reverse_k_group(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr || k == 1) {
+            return head;
+        }
+
+        ListNode dummy;
+        dummy.next = head;
+        ListNode* begin = &dummy;
+
+        int i = 0;
+        while (head != nullptr) {
+            i++;
+            if (i % k == 0) { // in this case, i == k
+                begin = reverse(begin, head->next);
+                head = begin->next;
+            } else {
+                head = head->next;
+            }
+        }
+        return dummy.next;
+    }
+
+private:
+    ListNode* reverse(ListNode* begin, ListNode* end) {
+        ListNode* curr = begin->next;
+        ListNode* prev = begin;
+        ListNode* first = curr;
+        ListNode* curr_next = nullptr;
+
+        while (curr != end) {
+            curr_next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = curr_next;
+        }
+        begin->next = prev;
+        first->next = curr;
+
+        return first;
+    }
+};
