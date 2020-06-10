2873a26b79d88de389a03154ba91bcbb1a5958f5
Aili Gong <aili.hgong@gmail.com>
2020-06-10 21:09:09 +0000

    Added fizz_buzz

---
+++ fizz_buzz.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> fizz_buzz(int n) {
+    vector<string> res;
+
+    if (n < 1) {
+        return res;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            res.push_back("FizzBuzz");
+        } else if (i % 3 == 0) {
+            res.push_back("Fizz");
+        } else if (i % 5 == 0) {
+            res.push_back("Buzz");
+        } else {
+            res.push_back(to_string(i));
+        }
+    }
+    return res;
+}
