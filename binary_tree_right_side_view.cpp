e9d434b338d72a885f13751950fad458b6060f90
Aili Gong <aili.hgong@gmail.com>
2020-06-12 21:14:21 +0000

    Finished binary_tree_right_side_view

--- binary_tree_right_side_view.cpp
+++ binary_tree_right_side_view.cpp
@@ -1,5 +1,46 @@
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
 
+class BinaryRightView {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
+
+        queue<TreeNode*> q;
+        q.emplace(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                if (i == size - 1) {
+                    res.push_back(node->val);
+                }
+
+                if (node->left != nullptr) {
+                    q.emplace(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.emplace(node->right);
+                }
+                q.pop();
+            }
+        }
+        return res;
+    }
+};
